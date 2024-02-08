acd4c4cbdb5c4ca40707ccf9cbfd413a7443abe3
Abel-Lagonell <alagonell1730@floridapoly.edu>
2024-02-08 14:19:14 -0500

    added conditions

--- queens/src/main.rs
+++ queens/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct Cell {
     row: u8,
@@ -7,8 +9,8 @@ pub struct Cell {
 impl Cell{
     pub fn new() -> Cell{
         Cell{
-            row:0,
-            col:0,
+            row:1,
+            col:1,
         }
     }
 
@@ -18,22 +20,63 @@ impl Cell{
     }
 }
 
-
-#[derive(Debug)]
-pub struct Board {
-    board: Vec<Cell>,
+impl fmt::Display for Cell{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let ascii = self.row + 64;
+        let letter = ascii as char;
+        write!(f, "({}, {})", letter, self.col )
+    }
 }
 
-impl Board{
-    pub fn new(size: usize) -> Board {
-        Board{
-            board: vec![Cell::new(); size],
+
+#[derive(Debug)]
+pub struct Queens {
+    placements: Vec<Cell>,
+    size: usize,
+}
+
+impl Queens{
+    pub fn new(size: usize) -> Queens {
+        Queens{
+            placements: vec![Cell::new(); size],
+            size: size,
         }
     }
 
+    pub fn test() -> Queens{
+        Queens { placements: vec![Cell::new(); 2], size: (2) }
+    }
 
+    pub fn get_placements(&self) -> &Vec<Cell>{
+        &self.placements
+    }
+
+    pub fn check_row_columns(&self, row: u8, col: u8) -> u8{
+        let mut count: u8 = 0;
+        for queen in self.placements {
+            if queen.row == row || queen.col == col{
+                count += 1; 
+            }
+        }
+        count
+    }
+
+    pub fn check_diagonals(&self, row:u8, col:u8) -> u8{
+        let mut count: u8 = 0;
+        for queen in self.placements{
+            for i in 0..self.size{
+                if (queen.row + queen.col == i as u8) && (row + col == i as u8){
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
 }
 
 fn main(){
-    print!("Hello World");
+    let q = Queens::test();
+    let placements = q.get_placements();
+    println!("{}", q.check_row_columns(1, 1));
+    println!("{}, {}", placements[0], placements[1])
 }
